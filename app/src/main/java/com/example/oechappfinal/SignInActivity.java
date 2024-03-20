a42200d54725a718fd7193afe3c51d3a7993b1a6
Sergey-05 <sayvix005@gmail.com>
2024-03-21 02:08:56 +0300

    signInAdd

--- app/src/main/java/com/example/oechappfinal/SignInActivity.java
+++ app/src/main/java/com/example/oechappfinal/SignInActivity.java
@@ -1,6 +1,16 @@
 package com.example.oechappfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +20,103 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private EditText pswField, emailField;
+    private Button logInBtn;
+    private TextView signup;
+    private CheckBox remCheck;
+    private ImageView visibilityBnt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        visibilityBnt = findViewById(R.id.pswVisibilityToggle);
+        pswField = findViewById(R.id.pswEdit);
+        emailField = findViewById(R.id.emailEdit);
+        remCheck = findViewById(R.id.rememberCheckbox);
+        logInBtn = findViewById(R.id.logInBtn);
+        signup = findViewById(R.id.signUpTxt);
+
+
+
+        emailField.addTextChangedListener(textWatcher);
+        pswField.addTextChangedListener(textWatcher);
+
+
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startLogIn = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(startLogIn);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startSignUp = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(startSignUp);
+            }
+        });
+
+        visibilityBnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText pswField = findViewById(R.id.pswEdit);
+                int inputType = pswField.getInputType();
+                if (inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD){
+                    pswField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    visibilityBnt.setImageResource(R.drawable.eye);
+                }else{
+                    pswField.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    visibilityBnt.setImageResource(R.drawable.eyeslash);
+                }
+                pswField.setSelection(pswField.getText().length());
+            }
+        });
+
+        TextView forgotTxt = findViewById(R.id.forgotTxt);
+        forgotTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgotActivity = new Intent(SignInActivity.this, ForgotActivity.class);
+                startActivity(forgotActivity);
+            }
         });
     }
+
+
+    private void checkFieldsNotEmpty(){
+        String email = emailField.getText().toString();
+        String password = pswField.getText().toString();
+
+        if (!email.isEmpty() && !password.isEmpty()){
+            logInBtn.setEnabled(true);
+            logInBtn.setBackgroundResource(R.drawable.primary_button_back);
+        } else {
+            logInBtn.setEnabled(false);
+            logInBtn.setBackgroundResource(R.drawable.unactive_btn);
+        }
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            checkFieldsNotEmpty();
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/oechappfinal/SignUpActivity.java
+++ app/src/main/java/com/example/oechappfinal/SignUpActivity.java
@@ -1,10 +1,24 @@
 package com.example.oechappfinal;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Html;
 import android.text.InputType;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +28,81 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText fullNameEditText, phoneNumberEditText, emailAddressEditText, passwordEditText, confirmPasswordEditText;
+    private CheckBox termsCheckBox;
+    private Button signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        fullNameEditText = findViewById(R.id.edit_text_full_name);
+        phoneNumberEditText = findViewById(R.id.edit_text_phone_number);
+        emailAddressEditText = findViewById(R.id.edit_text_email_address);
+        passwordEditText = findViewById(R.id.edit_text_password);
+        confirmPasswordEditText = findViewById(R.id.edit_text_confirm_password);
+        termsCheckBox = findViewById(R.id.check_terms);
+        signUpButton = findViewById(R.id.btn_signup);
+        TextView signin = findViewById(R.id.signIn);
+
+
+
+
+
+        fullNameEditText.addTextChangedListener(textWatcher);
+        phoneNumberEditText.addTextChangedListener(textWatcher);
+        emailAddressEditText.addTextChangedListener(textWatcher);
+        passwordEditText.addTextChangedListener(textWatcher);
+        confirmPasswordEditText.addTextChangedListener(textWatcher);
+        termsCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                checkFieldsForEmptyValues();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startSignIn = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(startSignIn);
+            }
+        });
+
+
+
+
         // Находим иконку видимости пароля и устанавливаем для нее слушатель щелчка
+
+        TextView termsView = findViewById(R.id.termsView);
+        // Используем HTML для форматирования текста
+        String htmlText = "<font color=" + getResources().getColor(R.color.gray_dark) + ">By ticking this box, you agree to our</font> <font color=" + getResources().getColor(R.color.warning_color) + "><a href=\"https://www.example.com\" style=\"text-decoration:none\">Terms and conditions and private policy</a></font>";
+        termsView.setText(Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY));
+
+        // Делаем ссылку кликабельной
+        termsView.setMovementMethod(LinkMovementMethod.getInstance());
+        termsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.example.com"));
+                 startActivity(intent);
+            }
+        });
+
         ImageView passwordVisibilityToggle = findViewById(R.id.password_visibility_toggle);
         ImageView confirmPasswordVisibilityToggle = findViewById(R.id.confirm_password_visibility_toggle);
+
+
 
         passwordVisibilityToggle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,4 +133,44 @@ public class SignUpActivity extends AppCompatActivity {
             editText.setSelection(editText.getText().length()); // Keep cursor at the end of the text
         }
     }
+
+
+    // Метод для проверки полей на пустоту и активации кнопки
+    private void checkFieldsForEmptyValues() {
+        String fullName = fullNameEditText.getText().toString();
+        String phoneNumber = phoneNumberEditText.getText().toString();
+        String emailAddress = emailAddressEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+        boolean termsChecked = termsCheckBox.isChecked();
+
+        if (!fullName.isEmpty() && !phoneNumber.isEmpty() && !emailAddress.isEmpty() &&
+                !password.isEmpty() && !confirmPassword.isEmpty() && termsChecked) {
+            // Если все поля заполнены и чекбокс выбран, активируем кнопку
+            signUpButton.setEnabled(true);
+            signUpButton.setBackgroundResource(R.drawable.primary_button_back); // Замените на вашу картинку синей кнопки
+        } else {
+            // Иначе делаем кнопку неактивной
+            signUpButton.setEnabled(false);
+            signUpButton.setBackgroundResource(R.drawable.unactive_btn); // Замените на вашу картинку неактивной кнопки
+        }
+    }
+
+    // Метод для изменения видимости пароля
+
+    // Слушатель для отслеживания изменений в полях ввода
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            checkFieldsForEmptyValues(); // Вызываем метод проверки полей на пустоту
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
 }
